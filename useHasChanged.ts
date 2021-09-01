58b0f34e1a96ab4dedb39929d3a04f2be2218bd3
Sasha <66020534+wectorgame@users.noreply.github.com>
2021-09-01 10:45:20 +0300

    Update useHasChanged.ts

--- useHasChanged.ts
+++ useHasChanged.ts
@@ -1,6 +1,5 @@
 import { useEffect, useRef } from 'react';
 
-// https://stackoverflow.com/questions/53446020/how-to-compare-oldvalues-and-newvalues-on-react-hooks-useeffect
 export const useHasChanged = (val: any, skipUndefined?: boolean) => {
   const prevVal = usePrevious(val);
   if (prevVal === undefined && skipUndefined) return false;
